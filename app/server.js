8af8fd073e1bb2701a261a4f2c3b71a8d519ad43
Colin Stern <cstern19@gmail.com>
2017-03-31 13:28:12 -0400

    fb5

--- app/server.js
+++ app/server.js
@@ -1,9 +1,9 @@
 import {readDocument, writeDocument, addDocument} from './database.js';
 
 /**
- * Emulates how a REST call is *asynchronous* -- it calls your function back
- * some time in the future with data.
- */
+* Emulates how a REST call is *asynchronous* -- it calls your function back
+* some time in the future with data.
+*/
 function emulateServerReturn(data, cb) {
   setTimeout(() => {
     cb(data);
@@ -11,19 +11,19 @@ function emulateServerReturn(data, cb) {
 }
 
 /**
- * Given a feed item ID, returns a FeedItem object with references resolved.
- * Internal to the server, since it's synchronous.
- */
+* Given a feed item ID, returns a FeedItem object with references resolved.
+* Internal to the server, since it's synchronous.
+*/
 function getFeedItemSync(feedItemId) {
   var feedItem = readDocument('feedItems', feedItemId);
   // Resolve 'like' counter.
   feedItem.likeCounter =
-    feedItem.likeCounter.map((id) => readDocument('users', id));
+  feedItem.likeCounter.map((id) => readDocument('users', id));
   // Assuming a StatusUpdate. If we had other types of
   // FeedItems in the DB, we would
   // need to check the type and have logic for each type.
   feedItem.contents.author =
-    readDocument('users', feedItem.contents.author);
+  readDocument('users', feedItem.contents.author);
   // Resolve comment author.
   feedItem.comments.forEach((comment) => {
     comment.author = readDocument('users', comment.author);
@@ -32,10 +32,10 @@ function getFeedItemSync(feedItemId) {
 }
 
 /**
- * Emulates a REST call to get the feed data for a particular user.
- * @param user The ID of the user whose feed we are requesting.
- * @param cb A Function object, which we will invoke when the Feed's data is available.
- */
+* Emulates a REST call to get the feed data for a particular user.
+* @param user The ID of the user whose feed we are requesting.
+* @param cb A Function object, which we will invoke when the Feed's data is available.
+*/
 export function getFeedData(user, cb) {
   // Get the User object with the id "user".
   var userData = readDocument('users', user);
